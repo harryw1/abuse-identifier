587a4d39545280dcdeee8a69337328b2969f3e7e
Harry Weiss <harrisonrweiss1@gmail.com>
2025-04-15 14:58:58 -0400

    added ruff as a package for linting and formatting

--- src/data-test.py
+++ src/data-test.py
@@ -1,10 +1,12 @@
-
 def main():
     import pandas as pd
 
-    df = pd.read_parquet("hf://datasets/tdavidson/hate_speech_offensive/data/train-00000-of-00001.parquet")
+    df = pd.read_parquet(
+        "hf://datasets/tdavidson/hate_speech_offensive/data/train-00000-of-00001.parquet"
+    )
 
     print(df.head())
+
 
 if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 def main():
     print("Hello from abuse-identifier!")
 
+
 # %% Cell 2
 if __name__ == "__main__":
     main()
